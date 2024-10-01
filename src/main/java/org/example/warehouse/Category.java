79452126e7662315b2c72974872ad0357af3263b
ArdaHalim2 <arda.halim@iths.se>
2024-10-01 09:12:57 +0200

    2

--- src/main/java/org/example/warehouse/Category.java
+++ src/main/java/org/example/warehouse/Category.java
@@ -1,4 +1,24 @@
 package org.example.warehouse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Category {
+    private final String name;
+    private static final Map<String, Category> instances = new HashMap<>();
+
+    private Category(String name) {
+        this.name = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+
+    public static Category of(String name) {
+        if (name == null) {
+            throw new IllegalArgumentException("Category name can't be null");
+        }
+        return instances.computeIfAbsent(name, Category::new);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/org/example/warehouse/ProductRecord.java
+++ src/main/java/org/example/warehouse/ProductRecord.java
@@ -1,4 +1,38 @@
 package org.example.warehouse;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 public class ProductRecord {
+    private final UUID uuid;
+    private final String name;
+    private final Category category;
+    private BigDecimal price;
+
+    public ProductRecord(UUID uuid, String name, Category category, BigDecimal price) {
+        this.uuid = uuid != null ? uuid : UUID.randomUUID();
+        this.name = name;
+        this.category = category;
+        this.price = price != null ? price : BigDecimal.ZERO;
+    }
+
+    public UUID uuid() {
+        return uuid;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public Category category() {
+        return category;
+    }
+
+    public BigDecimal price() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price != null ? price : BigDecimal.ZERO;
+    }
 }
